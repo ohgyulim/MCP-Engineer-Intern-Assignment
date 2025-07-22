b3a70fec449b2c347f75be426ea07024a67925c3
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-07-22 12:51:04 +0900

    feat: add return path

--- sec_filing_downloader.py
+++ sec_filing_downloader.py
@@ -13,8 +13,7 @@ def parse_args() -> argparse.Namespace:
 
     return parser.parse_args()
 
-
-def download_sec_filing(cik: str, year: str, filing_type: str, output_dir_path: str):
+def download_sec_filing(cik: str, year: str, filing_type: str, output_dir_path: str) -> str:
     # example json_data url: https://data.sec.gov/submissions/CIK0001018724.json
     headers = {"User-Agent": "MyApp your.email@domain.com"}
     url = f"https://data.sec.gov/submissions/CIK{cik}.json"
@@ -24,10 +23,12 @@ def download_sec_filing(cik: str, year: str, filing_type: str, output_dir_path: 
     
     # data is sorted By reportData
     accession_number = ""
+    primary_document = ""
     for index, type in enumerate(filings_recent["form"]):
         report_date = filings_recent["reportDate"][index]
         if (type == filing_type and report_date.startswith(year)):
             accession_number = filings_recent["accessionNumber"][index]
+            primary_document = filings_recent["primaryDocument"][index]
             break
         
     output_path = Path(output_dir_path)
@@ -37,7 +38,15 @@ def download_sec_filing(cik: str, year: str, filing_type: str, output_dir_path: 
     res = requests.get(download_url, headers=headers)
     
     with zipfile.ZipFile(io.BytesIO(res.content)) as zf:
-        zf.extractall(path=output_path)      
+        zf.extractall(path=output_path)
+    
+    return_path = ""
+    for path in output_path.glob("*"):
+        if path.name == primary_document:
+            return_path = path;
+    
+    return return_path
+    
 
 
 if __name__ == "__main__":
