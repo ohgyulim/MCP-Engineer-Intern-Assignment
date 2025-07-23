7e70d594f69ef57848fe6d89561395955598c2b2
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-07-23 11:07:50 +0900

    feat: integrate tools with FastMCP

---
+++ main.py
@@ -0,0 +1,22 @@
+from mcp.server.fastmcp import FastMCP
+from html_to_pdf import html_to_pdf
+from read_as_markdown import read_as_markdown
+from sec_filing_downloader import download_sec_filing
+
+mcp = FastMCP("SEC Filing Processor")
+
+
+@mcp.tool()
+def wrapped_sec_filing_downloader(cik, year, filing_type, output_dir_path):
+    return download_sec_filing(cik, year, filing_type, output_dir_path)
+
+@mcp.tool()
+def wrapped_html_to_pdf(input_file_path, output_file_path):
+    return html_to_pdf(input_file_path, output_file_path)
+
+@mcp.tool()
+def wrapped_read_as_markdown(input_file_path):
+    return read_as_markdown(input_file_path)
+
+if __name__ == "__main__":
+    mcp.run()
