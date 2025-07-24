9ecff54fa46b4e32f4fb7dec641f5a193552974d
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-07-24 22:45:06 +0900

    refactor: use pandas instead of loop

--- sec_filing_downloader.py
+++ sec_filing_downloader.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pandas as pd
 import requests
 import zipfile
 import io
@@ -34,30 +35,39 @@ def download_sec_filing(cik: str, year: str, filing_type: str, output_dir_path: 
     url = f"https://data.sec.gov/submissions/CIK{cik}.json"
     res = requests.get(url, headers=headers)
     data = res.json()
-    filings_recent = data["filings"]["recent"]
     
-    # data is sorted By reportData
-    accession_number = ""
-    primary_document = ""
-    for index, _filing_type in enumerate(filings_recent["form"]):
-        report_date = filings_recent["reportDate"][index]
-        if (_filing_type == filing_type and report_date.startswith(year)):
-            accession_number = filings_recent["accessionNumber"][index]
-            primary_document = filings_recent["primaryDocument"][index]
-            break
+    filings_recent_data = (
+        pd.DataFrame(data["filings"]["recent"])
+        .assign(
+            reportDate=lambda d: pd.to_datetime(d["reportDate"]),
+            reportYear=lambda d: d["reportDate"].dt.year,
+        )
+    )
+    target_row = (
+        filings_recent_data
+        .query(f"form == '{filing_type}' and reportYear == {year}")
+        .sort_values("reportDate", ascending=False)
+        .iloc[0]
+    )
+    accession_number = target_row["accessionNumber"]
+    primary_document = target_row["primaryDocument"]
         
     output_path = Path(output_dir_path)
     output_path.mkdir(parents=True, exist_ok=True)
     
     download_url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number.replace('-','')}/{accession_number}-xbrl.zip"
     res = requests.get(download_url, headers=headers)
-    
+
     with zipfile.ZipFile(io.BytesIO(res.content)) as zf:
         zf.extractall(path=output_path)
-    
+
     return_path = ""
     for path in output_path.glob("*"):
         if path.name == primary_document:
             return_path = path
     
     return return_path
+
+
+if __name__ == "__main__":
+    print(download_sec_filing("0001018724", "2024", "8-K", "html/amzn_2024_8_k"))
